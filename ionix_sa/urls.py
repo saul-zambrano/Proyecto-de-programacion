1662ae2006ef49169f4baf604820b9b5c89e17ce
Saul <zambranosaul50@gmail.com>
2022-09-08 15:00:11 -0500

    Creacion de las vistas y url de la aplicacion de productos

--- ionix_sa/urls.py
+++ ionix_sa/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productos.views import productos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', productos, name='productos')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,6 @@
+from unicodedata import name
 from django.shortcuts import render
 
 # Create your views here.
+def productos(request):
+    return render(request, 'productos.html')
