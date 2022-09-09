3134d800d6b97e926c1d21169035cd9973c9a092
Saul <zambranosaul50@gmail.com>
2022-09-09 14:00:55 -0500

    registrando los modelos creados

--- productos/admin.py
+++ productos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from productos import CategoriaProducto, Usuario, MetodoPago, Domicilio, Producto, Orden, OrdenDetalle, Carrito
 # Register your models here.
+admin.site.register(CategoriaProducto)
+admin.site.register(Usuario)
+admin.site.register(MetodoPago)
+admin.site.register(Domicilio)
+admin.site.register(Producto)
+admin.site.register(Orden)
+admin.site.register(OrdenDetalle)
+admin.site.register(Carrito)
