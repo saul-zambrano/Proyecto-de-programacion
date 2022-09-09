95bd65fabf965d090be1c2883f811843dba5c500
Saul <zambranosaul50@gmail.com>
2022-09-09 13:47:23 -0500

    creacion de los modelos de django

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CategoriaProducto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+
+
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=15)
+
+class MetodoPago(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=60)
+
+class Domicilio(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pais = models.CharField(max_length=60)
+    ciudad = models.CharField(max_length=60)
+    direccion = models.CharField(max_length=100)
+
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    cat_prod = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=70)
+    precio = models.FloatField()
+    stock = models.IntegerField()
+
+class Orden(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.CASCADE)
+    id_domicilio = models.ForeignKey(Domicilio, on_delete=models.CASCADE)
+    total = models.FloatField()
+
+class OrdenDetalle(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_orden = models.ForeignKey(Orden, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio = models.FloatField()
+
+class Carrito(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
