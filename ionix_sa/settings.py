a73104f408932fb34838de6e25fe2ac2f883f4c6
Saul <zambranosaul50@gmail.com>
2022-09-09 14:24:49 -0500

    conectando el proyecto al mysql

--- ionix_sa/settings.py
+++ ionix_sa/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ionix_sa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysqlclient',
+        'NAME': 'ionix_sa',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
