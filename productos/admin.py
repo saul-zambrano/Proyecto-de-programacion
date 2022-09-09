64b0fc4a5d116fc820c41e887ecf0c53b11c49ea
Saul <zambranosaul50@gmail.com>
2022-09-09 14:02:53 -0500

    Corrigiendo un error en la importacion de los modelos y tambien migrando las tablas a la base de datos

--- productos/admin.py
+++ productos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from productos import CategoriaProducto, Usuario, MetodoPago, Domicilio, Producto, Orden, OrdenDetalle, Carrito
+from productos.models import CategoriaProducto, Usuario, MetodoPago, Domicilio, Producto, Orden, OrdenDetalle, Carrito
 # Register your models here.
 admin.site.register(CategoriaProducto)
 admin.site.register(Usuario)
