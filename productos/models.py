cff2f6c1f5e5c67a118828e7b75adf036aa6db0c
Saul <zambranosaul50@gmail.com>
2022-09-11 12:38:48 -0500

    creando la seccion de marcadores

--- productos/models.py
+++ productos/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 class CategoriaProducto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=60)
--- productos/static/script/main.js
+++
@@ -1,7 +0,0 @@
-if (window.fetch != undefined) console.log('FETCH OK')
-else console.log('FETCH NOT WORKS!')
-
-fetch('')
-.then(res=>res.ok ? Promise.resolve(res) : Promise.reject(res))
-.then(res=>res.json ())
-.then(res=>console.log(res))
